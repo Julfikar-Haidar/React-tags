768f6720ccc230c2b4cf579d7ae909a6596056ef
Julfikar-Haidar <julfikar6262@gmail.com>
2020-10-22 13:59:22 +0600

    initial add

---
+++ src/component/test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+           <p>lorem</p> 
+        );
+    }
+}
+
+export default test;
